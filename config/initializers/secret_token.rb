136d9d5ead546f7aa1d6ab2cb425a75cf35e4948
Scott Kennedy <skennedy84@gmail.com>
2014-02-18 11:23:51 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EgoGiraffe::Application.config.secret_key_base = '3b17b8d14924f353e9465b914a3bb4b95582313b6330263564f4912886ced132eaf644ce0c1d6f328d6b531f84595e0d05b976a50fb81cc209241a5b2e86eded'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EgoGiraffe::Application.config.session_store :cookie_store, key: '_ego_giraffe_session'
