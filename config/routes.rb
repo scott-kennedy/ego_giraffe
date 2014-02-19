1e1bde044208e8b6b1a44875cd791520695348ce
Scott Kennedy <skennedy84@gmail.com>
2014-02-19 13:09:21 -0500

    wireframe for the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 EgoGiraffe::Application.routes.draw do
+  root 'static_pages#home'
+  
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
